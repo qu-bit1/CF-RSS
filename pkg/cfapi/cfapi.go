139d76a377cd80460c4ff0831e996b38e6b836df
qu-bit1 <103852686+qu-bit1@users.noreply.github.com>
2023-03-01 14:42:18 +0530

    created recent actions method, used http client,zap, created object

--- pkg/cfapi/cfapi.go
+++ pkg/cfapi/cfapi.go
@@ -1,9 +1,41 @@
 package cfapi
 
 import (
+	"fmt"
 	"github.com/qu-bit1/project_new/pkg/models"
+	"go.uber.org/zap"
+	"io"
+	"net/http"
 )
 
 type CodeforcesAPI interface {
 	RecentActions(maxCount int) ([]models.RecentAction, error)
+}
+
+type CodeforcesClient struct {
+	client http.Client
+}
+
+func (cfClient *CodeforcesClient) RecentActions(maxCount int) ([]models.RecentAction, error) {
+	resp, err := cfClient.client.Get("https://codeforces.com/api/recentActions?maxCount=30")
+	if err != nil {
+		zap.S().Errorf("error occurred while calling cfapi ; %v", err) //zap.S logs in a nice and pretty manner
+		return nil, err
+	}
+	defer resp.Body.Close()
+	data, err1 := io.ReadAll(resp.Body)
+	if err1 != nil {
+		fmt.Println("error occurred while reading body")
+		return nil, err1
+	}
+	zap.S().Info(string(data))
+
+	// TODO- unmarshall the data into models.recentActions ( will make a wrapper struct for that)
+	return nil, nil
+}
+
+// NewCodeforcesClient return an empty interface on which recent actions method can be applied
+func NewCodeforcesClient() CodeforcesAPI {
+	obj := new(CodeforcesClient)
+	return obj
 }
