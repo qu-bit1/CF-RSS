414711a6c96ddbc980e9334f4d823178280ea405
qu-bit1 <103852686+qu-bit1@users.noreply.github.com>
2023-03-01 17:07:38 +0530

    connected with mongoDB

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,49 +1,72 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
 	"fmt"
-	"github.com/qu-bit1/project_new/pkg/cfapi"
-	"go.uber.org/zap"
-	"log"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
+const uri = "mongodb://localhost:27017/"
+
 func main() {
-	fmt.Println("CF-RSS")
-	environment := "development"
-
-	var logger *zap.Logger
-	var loggerErr error
-	// new Development function prints logger and error in human friendly format where newP prints in json
-	if environment == "development" {
-		if logger, loggerErr = zap.NewDevelopment(); loggerErr != nil {
-			log.Fatalln(loggerErr)
-		}
-	} else {
-		if logger, loggerErr = zap.NewProduction(); loggerErr != nil {
-			log.Fatalln(loggerErr)
-		}
-	}
-	defer func(logger *zap.Logger) {
-		err := logger.Sync()
-		if err != nil {
-
-		}
-	}(logger)
-	zap.ReplaceGlobals(logger)
-	//zap.S().Info("these are some info logs")
-	//zap.S().Error("these are some error logs")
-	obj := cfapi.NewCodeforcesClient()
-	recentActions, err := obj.RecentActions(1)
+	// Use the SetServerAPIOptions() method to set the Stable API version to 1
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
 	if err != nil {
-		fmt.Println("error occurred while importing data from cfapi")
-		return
+		panic(err)
 	}
-
-	data, err1 := json.MarshalIndent(recentActions, "", " ")
-	if err1 != nil {
-		fmt.Println("error occurred while marshal indenting")
-		return
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	// Send a ping to confirm a successful connection
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
 	}
-	zap.S().Info(string(data))
+	fmt.Println("Pinged the primary node of the cluster. You successfully connected to MongoDB!")
 }
+
+//func main() {
+//	fmt.Println("CF-RSS")
+//	environment := "development"
+//
+//	var logger *zap.Logger
+//	var loggerErr error
+//	// new Development function prints logger and error in human friendly format where newP prints in json
+//	if environment == "development" {
+//		if logger, loggerErr = zap.NewDevelopment(); loggerErr != nil {
+//			log.Fatalln(loggerErr)
+//		}
+//	} else {
+//		if logger, loggerErr = zap.NewProduction(); loggerErr != nil {
+//			log.Fatalln(loggerErr)
+//		}
+//	}
+//	defer func(logger *zap.Logger) {
+//		err := logger.Sync()
+//		if err != nil {
+//
+//		}
+//	}(logger)
+//	zap.ReplaceGlobals(logger)
+//	//zap.S().Info("these are some info logs")
+//	//zap.S().Error("these are some error logs")
+//	obj := cfapi.NewCodeforcesClient()
+//	recentActions, err := obj.RecentActions(1)
+//	if err != nil {
+//		fmt.Println("error occurred while importing data from cfapi")
+//		return
+//	}
+//
+//	data, err1 := json.MarshalIndent(recentActions, "", " ")
+//	if err1 != nil {
+//		fmt.Println("error occurred while marshal indenting")
+//		return
+//	}
+//	zap.S().Info(string(data))
+//}
